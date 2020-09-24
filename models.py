20140fe5bdb048706badb294fc9ab509033d808b
Nikolai <nik.vagin1995@mail.ru>
2020-09-25 00:05:06 +0300

    create model Tag and setting foreignkey for model posts and tag

--- models.py
+++ models.py
@@ -7,13 +7,19 @@ def slugfy(s):
     return re.sub(pattern, '-', s)
 
 
+post_tags = db.Table('post_tags',
+                     db.Column('post_id', db.Integer, db.ForeignKey('posts.id')),
+                     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+                     )
+
+
 class Posts(db.Model):
-    """Модель постов"""
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150))
     slug = db.Column(db.String(150), unique=True)
     text = db.Column(db.Text)
     created = db.Column(db.DateTime, default=datetime.now())
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'),)
 
     def __init__(self, *args, **kwargs):
         super(Posts, self).__init__(*args, **kwargs)
@@ -25,4 +31,17 @@ class Posts(db.Model):
 
     def __repr__(self):
         return '<Post_id: {} title: {}'.format(self.id, self.title.lower())
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100))
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugfy(self.name)
+
+    def __repr__(self):
+        return '< Tag id: {}, name {}>'.format(self.id, self.name)
 
