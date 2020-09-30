8d8bcee71311685e9924f39d9b8bd673563359d2
Nikolai <nik.vagin1995@mail.ru>
2020-10-01 00:30:48 +0300

    creating func search

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,13 +1,17 @@
 from flask import Blueprint
 from flask import render_template
-
+from flask import request
 from models import Posts, Tag
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 @posts.route('/')
 def index():
-    posts = Posts.query.all()
+    q = request.args.get('query')
+    if q:
+        posts =  Posts.query.filter(Posts.title.contains(q) | Posts.text.contains(q)).all()
+    else:
+        posts = Posts.query.all()
     return render_template('posts/index.html', posts=posts)
 
 
@@ -21,7 +25,7 @@ def post_detail(slug):
 @posts.route('/tag/<slug>')
 def tag_detail(slug):
     tag = Tag.query.filter(Tag.slug==slug).first()
-    posts = tag.posts
+    posts = tag.posts.all()
     return render_template('posts/tag_detail.html', tag=tag, posts=posts)
 
 
