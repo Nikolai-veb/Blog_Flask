cc0311c58c1d5d3be6a489719caffacda06489e1
Nikolai <nik.vagin1995@mail.ru>
2020-10-02 00:43:20 +0300

    created forms and created views for form

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,9 +1,33 @@
 from flask import Blueprint
 from flask import render_template
 from flask import request
+
+from flask import url_for
+from flask import redirect
+
 from models import Posts, Tag
+from .forms import PostForm
+from app import db
 
 posts = Blueprint('posts', __name__, template_folder='templates')
+
+
+@posts.route('/create', methods=['POST', 'GET'])
+def create_posts():
+    if  request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+        try:
+            post = Posts(title=title, text=text)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print('SOME MISTAKE')
+        else:
+            return redirect(url_for('posts.index'))
+    form = PostForm()
+    return render_template('posts/create_posts.html', form=form)
+
 
 @posts.route('/')
 def index():
@@ -11,8 +35,11 @@ def index():
     if q:
         posts =  Posts.query.filter(Posts.title.contains(q) | Posts.text.contains(q)).all()
     else:
-        posts = Posts.query.all()
+        posts = Posts.query.order_by(Posts.created.desc())
     return render_template('posts/index.html', posts=posts)
+
+
+
 
 
 @posts.route('/<slug>')
